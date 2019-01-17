da1d23f66d3f1c7449933711e0a442212aaa8ca5
Mackenzie Weaver <macknz7@gmail.com>
2019-01-17 08:33:24 -0300

    Add files via upload
    Line 80 there is an example of how to download a video from a specified link and what the element looks like.
    
    mediaUrls( ) function now returns all urls which lead to meeting media.
    
    Line 49 you can see which CSS selectors find these elements. Then a for loop stores all of the urls into a list and returns it.

--- dmm.py
+++ dmm.py
@@ -5,6 +5,13 @@
 # imports
 import requests, bs4, webbrowser, pprint, os
 
+# Dummy dictionary for testing
+info = {'month': 'enero', 'year': '2019', 'dates': '28en-3febr', 'path':
+        'C:\\Users\\Mack W\\Documents\\Python\\personalProjects\\Meeting Media Downloader'}
+
+# where to save the media
+os.chdir(info['path'])
+
 # month, year, dates (Example: 7-13)
 def inputs():
     info = {'month': '', 'year': '', 'dates': '', 'path': ''}
@@ -19,8 +26,8 @@ def guia(info):
     month = info['month']
     year = info['year']
     dates = info['dates']    
-    base = 'https://www.jw.org/es/publicaciones/guia-actividades-reunion-testigos-jehova/'
-    ext = '%s-%s-mwb/programa-reunion-%sen/' % (month, year, dates)
+    ext = '/%s-%s-mwb/programa-reunion-%s/' % (month, year, dates)
+    base = 'https://www.jw.org/es/publicaciones/guia-actividades-reunion-testigos-jehova'
     url = base + ext
     res = requests.get(url)
     res.raise_for_status()
@@ -31,35 +38,63 @@ def guia(info):
         file.write(chunk)
     file.close()
 
-def media(info):
+def mediaUrls(info):
     month = info['month']
-    dates = info['dates'] 
+    dates = info['dates']
+    
     # make soup
     file = open(('%s %s workbook.txt' % (month, dates)), 'rb')
     meetingSoup = bs4.BeautifulSoup(file, features="lxml")
+    
+    # find elements
+    elems = []
+    elems += meetingSoup.select('a[class="pubSym-nwtsv"]') # introduction to bible books videos
+    elems += meetingSoup.select('a[class="pubSym-mwb19"]') # tesoros y nuestra vida cristiana
+    elems += meetingSoup.select('a[data-video]')           # videos when it says 'el video'
+    elems += meetingSoup.select('a[class="pubSym-thv"]')   # seamos mejores maestros
+    elems += meetingSoup.select('a[class="pubSym-jy"]')    # Jesus-The Way jy book
+    
     # find links
-    # elems = meetingSoup.select('a[data-video]') # finds videos when it says 'el video'
-    elems = meetingSoup.select('em')
-    for i in range(len(elems)):       
-        print(elems[i])
+    urls = []
+    for i in range(len(elems)):        
+        base = 'https://www.jw.org'
+        ext = elems[i].get('href')
+        if ext.startswith('/'):
+            url = base + ext
+        else:
+            url = ext
+        urls.append(url)
+    return urls
 
 # Main
-
-# get data
-#info = inputs()
-info = {'month': 'enero', 'year': '2019', 'dates': '7-13', 'path':
-        'C:\\Users\\Mack W\\Documents\\Python\\personalProjects\\Meeting Media Downloader'}
-
-# where to save the media
-os.chdir(info['path'])
-
-# Function calls
+# info = inputs()
 # guia(info)
-media(info)
+urls = mediaUrls(info)
+print(urls)
 
 # TODO
 '''
-line 42 - find out what element leads to media content
+
+'''
+
+# Example: download video
+'''
+dwn_link = 'https://download-a.akamaihd.net/files/media_periodical/8f/mwbv_E_201901_01_r720P.mp4'
+file_name = 'primera conversación.mp4' 
+rsp = requests.get(dwn_link)
+rsp.raise_for_status()
+file = open(file_name, 'wb')
+for chunk in rsp.iter_content(100000):
+    file.write(chunk)
+file.close()
+
+# Video element
+# <video
+# class="vjs-tech" id="vjs_video_721_html5_api" tabindex="-1"
+# preload="none" src="https://download-a.akamaihd.net/files/
+# media_periodical/8f/mwbv_E_201901_01_r720P.mp4"
+# poster="https://assetsnffrgf-a.akamaihd.net/assets/m/mwbv/univ/201901/art/mwbv_univ_201901_wsr_01_lg.jpg">
+# </video>
 '''
 
 
